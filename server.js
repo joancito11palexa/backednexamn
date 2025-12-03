f29c4d252f14abd70c4fdfc5a58c9d7b4196db13
joancito11palexa <hachetemele2@gmail.com>
2025-12-03 10:16:17 -0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-lite" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 app.post("/generate-text", async (req, res) => {
   try {
